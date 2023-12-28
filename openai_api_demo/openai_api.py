542ae20e881228172531bad81bb59ef4b984e56c
linshisancc <linshisancc@163.com>
2023-12-29 03:16:39 +0800

    feat: 更改path

--- openai_api_demo/openai_api.py
+++ openai_api_demo/openai_api.py
@@ -47,9 +47,9 @@ from sklearn.preprocessing import PolynomialFeatures
 EventSourceResponse.DEFAULT_PING_INTERVAL = 1000
 
 # chat-glm3模型
-MODEL_PATH = os.environ.get('GLM3_MODEL_PATH', '')
+MODEL_PATH = os.environ.get('GLM3_MODEL_PATH', '-')
 # m3e模型
-M3E_MODEL_PATH = os.environ.get('M3E_MODEL_PATH', '')
+M3E_MODEL_PATH = os.environ.get('M3E_MODEL_PATH', '-')
 TOKENIZER_PATH = os.environ.get("TOKENIZER_PATH", MODEL_PATH)
 
 @asynccontextmanager
